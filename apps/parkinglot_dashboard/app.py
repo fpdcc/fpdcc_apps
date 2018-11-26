8dc3b4476463519329e283e754011f83cf8c4519
glw <garretlewis@gmail.com>
2018-11-26 17:02:39 -0600

    initial attempt, still not completely functional.

--- apps/parkinglot_dashboard/app.py
+++ apps/parkinglot_dashboard/app.py
@@ -5,55 +5,95 @@ import dash_html_components as html
 import dash_table_experiments as dt
 import json
 import plotly
-import plotly.graph_objs as go
+from plotly import graph_objs as go
+from plotly.graph_objs import *
 import pandas as pd
 from sqlalchemy import create_engine
 import yaml
 
+mapbox_access_token = 'pk.eyJ1IjoiZ2x3IiwiYSI6IjdHTVp3eWMifQ.TXIsy6c3KnbqnUAeBawVsA'
+
 #############
 # ENV SETUP #
 #############
-config = yaml.safe_load(open("config.yaml"))
+# config = yaml.safe_load(open("config.yaml"))
 
 # postgresql db connection
-CREATEENGINE = config['postgresql']['CREATEENGINE']
+# CREATEENGINE = config['postgresql']['CREATEENGINE']
 
 ###################
 # create connection
 ###################
-engine = create_engine(CREATEENGINE)
+# engine = create_engine(CREATEENGINE)
+# sql = 'SELECT lot, sq_feet, geom from public.lots_table order by lot;'
 
+# gdf = gpd.GeoDataFrame.from_postgis(sql, con=engine, geom_col='geom' )
 
-df = pd.read_sql('SELECT * from public.lots_table order by lot;', con=engine)
-df = df.fillna('')
-df['sq_feet'] = df['sq_feet'].astype(int)
+df  = pd.read_csv('/home/garret/projects/fpdcc_apps/apps/parkinglot_dashboard/test_data/parkinglots_simplified_centroid_4326.csv')
+site_lat = list(df['lat'])
+site_lon = list(df['lon'])
+locations_name = list(df['lot'])
 
-df_subset = df[['lot', 'sq_feet']]
+external_stylesheet = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheet)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheet)
 app = dash.Dash()
 app.scripts.config.serve_locally=True
 
 app.layout = html.Div([
     html.H4('FPDCC Parking Lots'),
+    html.Div([
+        html.Div([
+            dcc.Graph(
+            	id = 'map',
+            	figure = dict(
+            		data = [dict(
+            			type = 'scattermapbox',
+                        lat = site_lat,
+            		    lon = site_lon,
+                        mode = 'markers'
+                        )],
+            		layout = dict(
+                        hovermode = "closest",
+            			height = 800,
+            			autosize = True,
+            			mapbox = dict(
+            				layers = [dict(
+            					type = 'point',
+            					color = '#265465'
+            				)],
+            				accesstoken = mapbox_access_token,
+            				center = dict(
+            		            lat = 41.808611,
+            		            lon = -87.888889
+            		        ),
+            		        zoom = 9,
+            		        style = 'light'
+            			)
+            		)
+            	)
+            )
+        ], className = 'six columns'),
+        html.Div([
+            dcc.Graph(
+                id = 'graph-lots'
+            )
+    ], className = 'six columns')
+    ], className = 'row'),
 
-    dcc.Graph(
-        id='graph-lots'
-        ),
-
-    dt.DataTable(
-        rows=df_subset.to_dict('records'),
-        columns=sorted(df_subset.columns),
-        row_selectable=True,
-        filterable=True,
-        sortable=True,
-        selected_row_indices=[],
-        id='datatable-lots'
-    ),
-    html.Div(id='selected-indexes'),
-], className="container")
+    html.Div([
+        dt.DataTable(
+            rows = df.to_dict('records'),
+            columns = sorted(df.columns.difference(['geom'])),
+            row_selectable = True,
+            filterable = True,
+            sortable = True,
+            selected_row_indices = [],
+            id = 'datatable-lots'
+        )
+    ]),
+    html.Div(id = 'selected-indexes'),
+], className = "container")
 
 @app.callback(
     Output('datatable-lots', 'selected_row_indices'),
@@ -75,40 +115,20 @@ def update_selected_row_indices(clickData, selected_row_indices):
      Input('datatable-lots', 'selected_row_indices')])
 def update_figure(rows, selected_row_indices):
     dff = pd.DataFrame(rows)
-    fig = plotly.tools.make_subplots(
-        rows=2, cols=1,
-        subplot_titles=('lot size 1', 'lot size 2',),
-        shared_xaxes=True)
-    marker = {'color': ['#0074D9']*len(dff)}
-    for i in (selected_row_indices or []):
-        marker['color'][i] = '#FF851B'
-    fig.append_trace({
-        'x': dff['lot'],
-        'y': dff['sq_feet'],
-        'type': 'bar',
-        'marker': marker
-    }, 1, 1)
-    fig.append_trace({
-        'x': dff['lot'],
-        'y': dff['sq_feet'],
-        'type': 'bar',
-        'marker': marker
-    }, 2, 1)
-    fig['layout']['showlegend'] = False
-    fig['layout']['height'] = 800
-    fig['layout']['margin'] = {
-        'l': 20,
-        'r': 20,
-        't': 20,
-        'b': 20
-    }
-    fig['layout']['yaxis2']['type'] = 'linear'
+    data = [go.Bar(
+            x = dff['lot'],
+            y = dff['sqft'],
+            width = 1,
+            name = 'Lot Square Feet'
+            )
+        ]
+    fig = go.Figure(data=data)
     return fig
 
 
-app.css.append_css({
-    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
-})
+#app.css.append_css({
+#    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+#})
 
 
 if __name__ == '__main__':
